b1504564c25d599369fd9f567f9186fc06948e95
APetraki <APetraki@croc.ru>
2025-02-08 19:01:24 +0300

    Версия spark заменена на 3.5.2, hive 4.0.1. shading  импортах. Закомментил неиспользуемый метод в AcidUtils

--- src/main/scala/com/qubole/shaded/hadoop/hive/ql/io/orc/OrcAcidUtil.scala
+++ src/main/scala/com/qubole/shaded/hadoop/hive/ql/io/orc/OrcAcidUtil.scala
@@ -1,18 +1,15 @@
 package com.qubole.shaded.hadoop.hive.ql.io.orc
 
-import com.qubole.shaded.hadoop.hive.ql.io.AcidUtils
-import org.apache.hadoop.fs.Path
-
 import java.util.regex.Pattern
 
 object OrcAcidUtil {
   val BUCKET_PATTERN = Pattern.compile("bucket_[0-9]{5}$")
 
-  def getDeleteDeltaPaths(orcSplit: OrcSplit): Array[Path] = {
-    assert(BUCKET_PATTERN.matcher(orcSplit.getPath.getName).matches())
-    val bucket = AcidUtils.parseBucketId(orcSplit.getPath)
-    assert(bucket != -1)
-    val deleteDeltaDirPaths = VectorizedOrcAcidRowBatchReader.getDeleteDeltaDirsFromSplit(orcSplit);
-    deleteDeltaDirPaths.map(deleteDir => AcidUtils.createBucketFile(deleteDir, bucket))
-  }
+//  def getDeleteDeltaPaths(orcSplit: OrcSplit): Array[Path] = {
+//    assert(BUCKET_PATTERN.matcher(orcSplit.getPath.getName).matches())
+//    val bucket = AcidUtils.parseBucketId(orcSplit.getPath)
+//    assert(bucket != -1)
+//    val deleteDeltaDirPaths = VectorizedOrcAcidRowBatchReader.getDeleteDeltaDirsFromSplit(orcSplit);
+//    deleteDeltaDirPaths.map(deleteDir => AcidUtils.createBucketFile(deleteDir, bucket))
+//  }
 }
